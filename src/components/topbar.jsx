1819ded6a378e9184d4b21ad09be0a94a5391864
Vishal Gowda K R <vishalgowda096@gmail.com>
2024-01-14 00:11:36 +0530

    changed TopBar a bit

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -13,9 +13,10 @@ export default function TopBar({topbar, b_r, p_x}) {
                 spread: 70,
                 origin: { y: 0.6 }
             });
+
             await setCookie('signedUp', 'true', 365);
+
         } else {
-            // Show a pop up saying you have already signed up
             alert('You have already signed up!');
         }
     }
@@ -29,11 +30,11 @@ export default function TopBar({topbar, b_r, p_x}) {
         root.style.setProperty('--padding-x', p_x)
     });
     return (
-        <div className="h-16 .w-full bg-neutral-300  shadow-md flex flex-row justify-center py-2 items-center px-32" id='topbar'>
+        <div className="h-16 .w-full bg-neutral-300  shadow-md flex flex-row justify-between py-2 items-center px-32" id='topbar'>
             <div className="topbar-element text-3xl max-sm:text-sm m-1 mx-7" id='logo'>
                 SkillSync
             </div>
-            <input type='text' placeholder='Search for a course' className="bg-zinc-100 w-1/5 max-sm:w-1/6 p-3 h-11  rounded-3xl border border-black mx-7" id='SearchBar'/>
+            <input type='text' placeholder='Search for a course' className="bg-zinc-100 w-1/2 max-sm:w-1/6 p-3 h-11  rounded-3xl border border-black mx-7" id='SearchBar'/>
             <button className="bg-white p-2 h-4/5  text-sm border border-black text-black mx-7" id='teach'>Teach on Skill Sync</button>
             <button className='bg-white p-2 h-4/5  border border-black mx-7' id='login'>Log in</button>
             <button className='bg-black p-2 h-4/5  text-white mx-7' id='signup' onClick={signupClick}>Sign up</button>
