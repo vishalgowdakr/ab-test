f53e4724d1ca9d5a54613cba3eea5efa5ecbe127
Vishal Gowda K R <vishalgowda096@gmail.com>
2024-01-25 15:09:21 +0530

    some_changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,32 @@ import banner1 from './assets/document.jpeg'
 import banner2 from './assets/Designer (2).png'
 import banner3 from './assets/Designer (3).png'
 import Companies from './components/companies';
-import { useState, useEffect} from 'react';
-import { getCookie, setCookie } from './cookie';
+import { useState, useEffect } from 'react';
+import { getCookie, setCookie } from './utils/cookie';
 
 
 function App() {
 
   const neutral = {
-    banner:  banner1,
+    theme: 'neutral',
+    banner: banner1,
     background: '#F4F4F4',
     topbar: 'rgb(212, 212, 212)',
-    b_r: '0px',
+    b_r: '0p)x',
     p_x: '8px'
-    
+
   }
   const dark = {
-    banner:  banner2,
+    theme: 'dark',
+    banner: banner2,
     background: '#1F1F1F',
     topbar: 'rgb(151,116,48)',
     b_r: '20px',
     p_x: '10px',
   }
   const light = {
-    banner:  banner3,
+    theme: 'light',
+    banner: banner3,
     background: '#FFFFFF',
     topbar: 'rgb(94,28,196)',
     b_r: '20px',
@@ -36,48 +39,48 @@ function App() {
   // const [newUser, setNewUser] = useState(false);
   var newUser = false;
 
-  useEffect(()=>{
+  useEffect(() => {
     const storedTheme = getCookie('colourScheme');
-    switch(storedTheme){
+    switch (storedTheme) {
       case 'neutral': setTheme(neutral);
-      break;
+        break;
       case 'dark': setTheme(dark);
-      break;
+        break;
       case 'light': setTheme(light)
-      break;
+        break;
       default: newUser = true;
-      break;
+        break;
     }
-  },[]);
+  }, []);
 
-  useEffect(()=>{
-  if(newUser === true){
-  const randomNumber = Math.floor(Math.random()*3)
-  switch(randomNumber){
-    case 0: setTheme(neutral);
-            setCookie('colourScheme', 'neutral', 365)
-    break;
-    case 1: setTheme(dark);
-            setCookie('colourScheme', 'dark', 365)
-    break;
-    case 2: setTheme(light);
-            setCookie('colourScheme', 'light', 365)
-    break;
-    default: setTheme(neutral);
-            setCookie('colourScheme', 'neutral', 365)
-  }
-}
+  useEffect(() => {
+    if (newUser === true) {
+      const randomNumber = Math.floor(Math.random() * 3)
+      switch (randomNumber) {
+        case 0: setTheme(neutral);
+          setCookie('colourScheme', 'neutral', 365)
+          break;
+        case 1: setTheme(dark);
+          setCookie('colourScheme', 'dark', 365)
+          break;
+        case 2: setTheme(light);
+          setCookie('colourScheme', 'light', 365)
+          break;
+        default: setTheme(neutral);
+          setCookie('colourScheme', 'neutral', 365)
+      }
+    }
 
-},[newUser]);
+  }, [newUser]);
 
   return (
-  <div className='flex-1 flex-col justify-items-center max-h-full'>
-    <TopBar topbar={theme.topbar} b_r={theme.b_r} p_x={theme.p_x}/>
-    <div className='flex flex-row  items-center my-10'>
-    <img src={theme.banner} alt='banner' className='my-10'/>
-    <Companies />
-    </div>
-  </div>  );
+    <div className='flex-1 flex-col justify-items-center max-h-full'>
+      <TopBar topbar={theme.topbar} b_r={theme.b_r} p_x={theme.p_x} theme={theme.theme} />
+      <div className='flex flex-row  items-center my-10'>
+        <img src={theme.banner} alt='banner' className='my-10' />
+        <Companies />
+      </div>
+    </div>);
 }
 
 export default App;
--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,47 +1,52 @@
 import { useEffect } from 'react'
 import './topbar.css'
 import confetti from 'canvas-confetti';
-import { getCookie, setCookie } from '../cookie';
+import { getCookie, setCookie } from '../utils/cookie';
+import { sendPostRequest } from '../utils/sendAnalytics';
 
-export default function TopBar({topbar, b_r, p_x}) {
-    const signupClick = async () => {
-        const isSignedUp = await getCookie('signedUp');
-        console.log(isSignedUp);
-        if (isSignedUp !== true) {
-            await confetti({
-                particleCount: 100,
-                spread: 70,
-                origin: { y: 0.6 }
-            });
+export default function TopBar({ topbar, b_r, p_x, theme }) {
+  const signupClick = async () => {
+    const isSignedUp = await getCookie('signedUp');
+    if (isSignedUp !== true) {
+      await confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+      });
 
-            await setCookie('signedUp', 'true', 365);
+      setCookie('signedUp', 'true', 365);
+      const analytics = JSON.stringify({
+        'theme': theme,
+      });
+      const response = sendPostRequest(analytics);
+      console.log(response);
 
-        } else {
-            alert('You have already signed up!');
-        }
+    } else {
+      alert('You have already signed up!');
     }
-    useEffect(()=>{
-        const root = document.documentElement;
+  }
+  useEffect(() => {
+    const root = document.documentElement;
 
-        // Update the value of the --main-color variable
-        root.style.setProperty('--main-color', topbar
-        );
-        root.style.setProperty('--border-radius', b_r);
-        root.style.setProperty('--padding-x', p_x)
-    });
-    return (
-        <div className="h-16 .w-full bg-neutral-300  shadow-md flex flex-row justify-between py-2 items-center px-32" id='topbar'>
-            <div className="topbar-element text-3xl max-sm:text-sm m-1 mx-7" id='logo'>
-                SkillSync
-            </div>
-            <input type='text' placeholder='Search for a course' className="bg-zinc-100 w-1/2 max-sm:w-1/6 p-3 h-11  rounded-3xl border border-black mx-7" id='SearchBar'/>
-            <button className="bg-white p-2 h-4/5  text-sm border border-black text-black mx-7" id='teach'>Teach on Skill Sync</button>
-            <button className='bg-white p-2 h-4/5  border border-black mx-7' id='login'>Log in</button>
-            <button className='bg-black p-2 h-4/5  text-white mx-7' id='signup' onClick={signupClick}>Sign up</button>
-            <span id='' className='border-black border h-4/5 w-10 flex justify-center items-center bg-white mx-7'>
-            <button id='' className='material-symbols-outlined'>
-            language
-            </button></span>
-        </div>
-    )
+    // Update the value of the --main-color variable
+    root.style.setProperty('--main-color', topbar
+    );
+    root.style.setProperty('--border-radius', b_r);
+    root.style.setProperty('--padding-x', p_x)
+  });
+  return (
+    <div className="h-16 .w-full bg-neutral-300  shadow-md flex flex-row justify-between py-2 items-center px-32" id='topbar'>
+      <div className="topbar-element text-3xl max-sm:text-sm m-1 mx-7" id='logo'>
+        SkillSync
+      </div>
+      <input type='text' placeholder='Search for a course' className="bg-zinc-100 w-1/2 max-sm:w-1/6 p-3 h-11  rounded-3xl border border-black mx-7" id='SearchBar' />
+      <button className="bg-white p-2 h-4/5  text-sm border border-black text-black mx-7" id='teach'>Teach on Skill Sync</button>
+      <button className='bg-white p-2 h-4/5  border border-black mx-7' id='login'>Log in</button>
+      <button className='bg-black p-2 h-4/5  text-white mx-7' id='signup' onClick={signupClick}>Sign up</button>
+      <span id='' className='border-black border h-4/5 w-10 flex justify-center items-center bg-white mx-7'>
+        <button id='' className='material-symbols-outlined'>
+          language
+        </button></span>
+    </div>
+  )
 }
